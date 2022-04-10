912fcc76a29d5b05f180bcc4f79d08357854c0b4
NelsonDaniel <nd.ta@hotmail.com>
2022-04-10 00:12:39 -0400

    parsing finished

--- chrome-extension/getting-started/popup.js
+++ chrome-extension/getting-started/popup.js
@@ -28,22 +28,27 @@ function setPageBackgroundColor() {
         }
     }
 
-
+    function parseTimeDetails(details) {
+      const location_a = details.getElementsByTagName('a')[0];
+      const location_link = location_a.href;
+      const building  = location_a.innerHTML;
+      const time_details = details.textContent.split(' ');
+      const days = time_details[0];
+      const start_time = time_details[1];
+      const end_time = time_details[3];
+      return {building, location_link, days, start_time, end_time};
+    }
+  
+    function parseCourseDiv(div) {
+      const rows = div.getElementsByTagName('tr');
+      const details = rows[0].getElementsByTagName('td');
+      const subject = details[4].textContent;
+      const course_number = details[5].textContent;
+      const time_details = parseTimeDetails(details[9]);
+      const parsedCourseDetails = {subject, course_number,...time_details};
+      return parsedCourseDetails;
+    }
   });
-
-  function parseCourseDiv(div) {
-    const rows = div.getElementsByTagName('tr');
-    const details = rows[0].getElementsByTagName('td');
-    const subject = details[4];
-    const course_number = details[5];
-    const time_details = parseTimeDetails(details[9]);
-
-    return {subject, course_number, days, time, link, bldg};
-  }
-
-  function parseTimeDetails() {
-    return "a", "b", "c", "d";
-  }
 }
 
 
