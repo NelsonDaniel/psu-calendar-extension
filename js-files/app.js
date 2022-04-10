1c03b860a658207592394336c719d2bba09cdc07
Shivank Vatsal <shivankvatsal@gmail.com>
2022-04-09 23:27:25 -0400

    waste

--- js-files/app.js
+++ js-files/app.js
@@ -1,3 +1,5 @@
+//const { OutputFileType } = require("typescript");
+
 var string1 = "http://www.google.com/maps/place/40.8004449,-77.8605032";
 var string2 = "http://www.google.com/maps/place/40.7913296,-77.8698712";
 var lat1 = string1.split('/')[5].split(',')[0]
@@ -12,15 +14,21 @@ var directionsService = new google.maps.DirectionsService();
 var request = {
     origin: origin, // LatLng|string
     destination: destination, // LatLng|string
-    travelMode: google.maps.DirectionsTravelMode.TRANSIT
+    travelMode: google.maps.DirectionsTravelMode.WALKING
 };
 
 
 directionsService.route( request, function( response, status ) {
 
     if ( status === 'OK' ) {
+        const output = document.querySelector('#output');
         var point = response.routes[ 0 ].legs[ 0 ];
         console.log(point);
+        console.log(parseInt(point.duration.text.split(' ')[0]));
+        if(parseInt(point.duration.text.split(' ')[0]) > 15){
+            document.innerHTML("<div>Here is some text</div>");
+        }
+        
     }
 }
  );
